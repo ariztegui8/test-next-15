46ed1a858805e58c62cdf3a5ffad3942ad0377e4
ariztegui8 <jorgeariztegui8@gmail.com>
2024-11-08 17:21:11 -0300

    add carrousel flicking

--- src/components/FlickingCarrousel.tsx
+++ src/components/FlickingCarrousel.tsx
@@ -1,17 +1,9 @@
-'use client'
-import Flicking, { MoveEvent, WillChangeEvent, ViewportSlot } from "@egjs/react-flicking";
+import Flicking, { MoveEvent, ViewportSlot, WillChangeEvent } from "@egjs/react-flicking";
 import "@egjs/react-flicking/dist/flicking.css";
-import { Arrow } from "@egjs/flicking-plugins";
 
 const FlickingCarrousel = () => {
-
-  const arrowPlugin = new Arrow({
-    prevEl: ".flicking-arrow-prev",
-    nextEl: ".flicking-arrow-next"
-  });
-
   return (
-    <div>
+    <div >
       <Flicking
         viewportTag="div"
         cameraTag="div"
@@ -24,18 +16,20 @@ const FlickingCarrousel = () => {
         }}
         horizontal={true}
         circular={true}
-        plugins={[arrowPlugin]}
       >
         <div style={{ width: "100%", height: "200px", backgroundColor: "lightblue" }}>Panel 0</div>
         <div style={{ width: "100%", height: "200px", backgroundColor: "lightcoral" }}>Panel 1</div>
         <div style={{ width: "100%", height: "200px", backgroundColor: "lightgreen" }}>Panel 2</div>
         <ViewportSlot>
-          <span className="flicking-arrow-prev" style={{ cursor: "pointer" }}>←</span>
-          <span className="flicking-arrow-next" style={{ cursor: "pointer" }}>→</span>
-        </ViewportSlot>
+        <span className="flicking-arrow-prev"></span>
+        <span className="flicking-arrow-next"></span>
+      </ViewportSlot>
       </Flicking>
-    </div>
-  );
-};
 
-export default FlickingCarrousel;
+      
+    </div>
+
+  )
+}
+
+export default FlickingCarrousel
